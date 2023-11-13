6f76329570f21429c546e4b9076f9f0ca01177e3
Gaanj <Dmytrobozescyl@gmail.com>
2023-11-13 17:15:35 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,18 @@
+// Функція filterArray приймає масив чисел (numbers) та значення (value) як параметри
 function filterArray(numbers, value) {
-    return numbers.filter((number) => number > value);
+  // Створюємо новий порожній масив для результату
+  let result = [];
+  // Проходимо по всіх елементах масиву numbers
+  for (let number of numbers) {
+    // Якщо число більше за value, додаємо його до масиву result
+    if (number > value) {
+      result.push(number);
+    }
   }
+  // Повертаємо масив result
+  return result;
+}
+//альтернативний варіант вирішення завдання:
+// function filterArray(numbers, value) {
+//   return numbers.filter((number) => number > value);
+// }

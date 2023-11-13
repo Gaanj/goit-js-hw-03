830e9359e56d2242d7e426647347d3cbd88547ad
Gaanj <Dmytrobozescyl@gmail.com>
2023-11-13 14:19:46 +0200

    upd

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,6 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    let a = pricePerDroid * quantity;
-    if (a > customerCredits) {
-        return ('Insufficient funds!')
-    }
-    else if (a <= customerCredits) {
-        return (`You ordered ${quantity} droids worth ${pricePerDroid * quantity} credits!`)
-    }
+let a = [];
+function slugify(title){
+    a = title.split(" ");
+    let b = a.join("-");
+    return b;
 }
-console.log(makeTransaction(5, 3000, 23000));
-console.log(makeTransaction(3, 1000, 15000));
-console.log(makeTransaction(10, 5000, 8000));
-console.log(makeTransaction(8, 2000, 10000));
-console.log(makeTransaction(10, 500, 5000));
